bd541341813d029d2aa53b03d9f1e537b2c76413
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-15 15:41:52 +0545

    Completed Scenario: Get Age by Name and Country ID

--- features/getAgeFromName.feature
+++ features/getAgeFromName.feature
@@ -4,4 +4,11 @@ Feature: Get age from name, country
   Scenario: Find age from the name only
     When the user sends a request to apify with name 'Michael'
     Then the response code should be '200'
-    And the response body should contain age
+    And the response body should contain the name 'Michael' and valid age
+
+
+  Scenario: Find age from name and country
+    When the user sends a request to apify with name 'Kiran' and country id 'US'
+    Then the response code should be '200'
+    And the response body should contain the name 'Kiran' and valid age
+    And the response body should contain country id 'US'
--- stepDefinitions/getAgeFromName.js
+++ stepDefinitions/getAgeFromName.js
@@ -14,6 +14,18 @@ When(
   }
 );
 
+When(
+  "the user sends a request to apify with name {string} and country id {string}",
+  async function (customName, countryId) {
+    response = await context.get("", {
+      params: {
+        name: customName,
+        country_id: countryId,
+      },
+    });
+  }
+);
+
 Then("the response code should be {string}", async function (statusCode) {
   assert.equal(
     response.status(),
@@ -22,11 +34,35 @@ Then("the response code should be {string}", async function (statusCode) {
   );
 });
 
-Then("the response body should contain age", async function () {
-  const responseBody = await response.json();
-  assert.equal(
-    typeof responseBody.age,
-    "number",
-    `Expected Number but got ${typeof responseBody.age}`
-  );
-});
+Then(
+  "the response body should contain the name {string} and valid age",
+  async function (expectedName) {
+    const responseBody = await response.json();
+
+    // checking that the response has the same name which was in the request
+    assert.equal(
+      expectedName,
+      responseBody.name,
+      `Expected name ${expectedName} but found ${responseBody.name}`
+    );
+
+    // checking that the response has found a valid age
+    assert.equal(
+      typeof responseBody.age,
+      "number",
+      `Expected Number but got ${typeof responseBody.age}\n This is because no such data was found that matched the request params`
+    );
+  }
+);
+
+Then(
+  "the response body should contain country id {string}",
+  async function (countryId) {
+    const response_country_id = (await response.json()).country_id;
+    assert.equal(
+      response_country_id,
+      countryId,
+      `Expected Response ${countryId} but found ${response_country_id}`
+    );
+  }
+);
