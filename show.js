e214dc266d12e4bf616c16831644bc5ec5bc90e2
Kiran Adhikari <kiranadh1452@gmail.com>
2022-06-17 17:23:06 +0545

    Check for accessibility of env vars from github secrets

---
+++ show.js
@@ -0,0 +1,8 @@
+const baseUrl = process.env.BASE_URL;
+console.log(baseUrl);
+
+if (baseUrl == "Jankari") {
+  console.log("I know your secret hahahahaha");
+} else {
+  console.log("I donot know your secret");
+}
